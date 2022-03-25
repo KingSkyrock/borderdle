60b98b454e5324ef3563f6f78bd104ebed875193
underscorelior <newlioryt@gmail.com>
2022-03-24 22:25:05 -0700

    Ok

--- src/components/TopBar/index.js
+++ src/components/TopBar/index.js
@@ -1,10 +1,10 @@
 import React from "React";
-import { BsQuestionSquare } from "react-icons/bs";
+import { BiQuestionMark } from "react-icons/bi";
 const TopBar = () => {
   return (
 	<div className="lgw flex justify-center">
 		<div className="flex flex-row w-fit border-b-2 px-3 border-[#195234] ">
-			<BsQuestionSquare size="48"/>
+			<BiQuestionMark/>
 			<h3 className="tracking-wide">BORDER<span className="lgc">DLE</span></h3>
 		</div>
 	</div>
