1c3480530addf997f207a6d2bdfc9280d8c36498
underscorelior <newlioryt@gmail.com>
2022-03-26 13:23:15 -0700

    Add Basic Modal, and add getpaths.py

---
+++ data/getpaths.py
@@ -0,0 +1,33 @@
+import json
+from urllib.request import urlopen
+import urllib
+xddd={}
+with open("data/cq copy 2.json", 'r+', encoding="utf-8") as svg: 
+    s=json.load(svg)
+with open("data/cq copy.json", 'r+', encoding="utf-8") as short: 
+    f=json.load(short)
+for x in range(len(s)):
+    try:
+        url=s[x]["flags"]
+        req = urllib.request.Request(url)
+        with urllib.request.urlopen(req) as fg:
+            fg=fg.read().decode('utf-8')
+            fg=fg.replace('''<?xml version="1.0" standalone="no"?>
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 20010904//EN"
+ "http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd">
+<svg version="1.0" xmlns="http://www.w3.org/2000/svg"
+ width="1024.000000pt" height="1024.000000pt" viewBox="0 0 1024.000000 1024.000000"
+ preserveAspectRatio="xMidYMid meet">
+<metadata>
+Created by potrace 1.10, written by Peter Selinger 2001-2011
+</metadata>
+<g transform="translate(0.000000,1024.000000) scale(0.100000,-0.100000)"
+fill="#000000" stroke="none">''',"").replace("""</g>
+</svg>""","").replace("\n\n","").replace("\n"," ").replace('"',"'")
+        xddd[f'{s[x]["name"].lower()}']=fg
+    except urllib.error.HTTPError:
+        print(f[x]["short"])
+# print(xddd)
+with open("paths.json","r+",encoding="utf-8") as xdddddd:
+    xdddddd.write(json.dumps(xddd))
+    xdddddd.close()
--- src/components/TopBar/InfoBtn.js
+++ src/components/TopBar/InfoBtn.js
@@ -3,6 +3,28 @@ import ReactModal from 'react-modal';
 import { Twemoji } from "@teuteuf/react-emoji-render";
 import { BsQuestionSquare } from "react-icons/bs";
 
+const modalSize ={
+	content:{
+		top: '50%',
+		left: '50%',
+		right: 'auto',
+		bottom: 'auto',
+		transform: 'translate(-50%, -50%)',
+		paddingTop: '35px',
+		paddingLeft: '45px',
+		paddingRight: '45px',
+		border: '0px',
+		height: '70%',
+		width: '35%',
+		minWidth: '350px',
+		borderRadius: '25px',
+		boxShadow: '0px 10px 20px 5px rgba(0, 0, 0, 0.3)',
+		backgroundColor: '1d8543'
+	},
+	overlay: {
+	   background: 'rgba(0, 0, 0,  0.3)'
+	}
+}
 export default class InfoBtn extends React.Component {
 	constructor () {
 	  super();
@@ -28,10 +50,13 @@ export default class InfoBtn extends React.Component {
 		  <button onClick={this.handleOpenModal}><BsQuestionSquare/></button>{/*<Twemoji text="❓"/>*/}
 		  <ReactModal 
 			 isOpen={this.state.showModal}
-			 contentLabel="How to play"
-			 overlayElement="<div>HELLO</div>"
+			 onRequestClose={this.handleCloseModal}
+           	 shouldCloseOnOverlayClick={true}
+			 style={modalSize}
 		  >
-			<button onClick={this.handleCloseModal}>Close Modal</button>
+			<div>
+				<h3 className='modaltext'>How to play <span className='tracking-wide text-bold'>BORDER<span className='text-[#195234]'>DLE</span></span></h3>
+			</div>
 		  </ReactModal>
 		</>
 	  );
