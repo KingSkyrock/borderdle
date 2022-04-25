4a4e1e5f88017efa775b55b0fc38f32587c914c7
underscorelior <newlioryt@gmail.com>
2022-04-25 11:57:09 -0700

    Fix tailwind complaining about updating purge/content

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,7 @@ const colors = require('tailwindcss/colors')
 
 module.exports = {
     enabled: true,
-    mode: 'jit',
-    purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+    content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
     darkMode: 'class',
     theme: {
         extend: {
