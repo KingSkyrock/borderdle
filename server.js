64b1f455679990b50da7efaed95574408618ba9b
Luke Wang <48535279+KingSkyrock@users.noreply.github.com>
2024-01-03 01:17:01 -0800

    Fixed and cleaned up backend

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import bodyParser from "body-parser";
 import schedule from "node-schedule";
 import fs from "node:fs/promises";
 import express from "express";
+import { fileURLToPath } from 'url';
 
 let country = null;
 newCountry();
@@ -12,17 +13,7 @@ newCountry();
 const isProduction = process.env.NODE_ENV === "production";
 const port = process.env.PORT || 5173;
 const base = process.env.BASE || "/";
-const currentModuleURL = new URL(import.meta.url);
-const DIST_DIR = path.join(path.dirname(currentModuleURL.pathname), "../dist");
-const HTML_FILE = path.join(DIST_DIR, "index.html");
-
-const templateHtml = isProduction
-  ? await fs.readFile("./dist/client/index.html", "utf-8")
-  : "";
-
-const ssrManifest = isProduction
-  ? await fs.readFile("./dist/client/.vite/ssr-manifest.json", "utf-8")
-  : undefined;
+const DIST_DIR = path.resolve(path.dirname(fileURLToPath(import.meta.url)), 'dist/client');
 
 const app = express();
 
@@ -36,12 +27,10 @@ if (!isProduction) {
   });
   app.use(vite.middlewares);
 } else {
-  const compression = (await import("compression")).default;
-  app.use(compression());
+  app.use(express.static(DIST_DIR, { index: false }));
 }
 
 app.use(compression());
-app.use(express.static(DIST_DIR));
 
 app.use(
   bodyParser.urlencoded({
@@ -77,27 +66,20 @@ app.post("/getAnswer", (req, res) => {
 
 app.use("*", async (req, res) => {
   try {
-    const url = req.originalUrl.replace(base, "");
-
-    let template;
-    let render;
-    if (!isProduction) {
-      // Always read fresh template in development
-      template = await fs.readFile("./index.html", "utf-8");
-      template = await vite.transformIndexHtml(url, template);
-      render = (await vite.ssrLoadModule("/src/entry-server.jsx")).render;
+    if (isProduction) {
+      const template = await fs.readFile('./dist/client/index.html', 'utf-8');
+      const { render } = await import('./dist/server/entry-server.js');
+  
+      const html = template.replace(`<!--outlet-->`, render);
+      res.status(200).set({ 'Content-Type': 'text/html' }).end(html);
     } else {
-      template = templateHtml;
-      render = (await import("./dist/server/entry-server.js")).render;
+      const url = req.originalUrl;
+      const template = await vite.transformIndexHtml(url, await fs.readFile('index.html', 'utf-8'));
+      const { render } = await vite.ssrLoadModule('/src/entry-server.jsx');
+  
+      const html = template.replace(`<!--outlet-->`, render);
+      res.status(200).set({ 'Content-Type': 'text/html' }).end(html);
     }
-
-    const rendered = await render(url, ssrManifest);
-
-    const html = template
-      .replace(`<!--app-head-->`, rendered.head ?? "")
-      .replace(`<!--app-html-->`, rendered.html ?? "");
-
-    res.status(200).set({ "Content-Type": "text/html" }).end(html);
   } catch (e) {
     vite?.ssrFixStacktrace(e);
     console.log(e.stack);
